b68a924ffb88be17bfc80f40aa3281664885365f
Andres Escobar <andresescobarv24@gmail.com>
2024-12-15 13:02:38 +1100

    app changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
         
-car_data = pd.read_csv('../vehicles_us.csv') # leer los datos
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
 st.header('Analisis proyecto 6')
 
